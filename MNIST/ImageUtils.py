62bc995909e37055ffb009e5d4b74efa0ed53e89
Manish Sinha <manish.sinha@xamarin.com>
2018-06-14 23:28:38 -0400

    [MNIST] Added dataset loading and visualization

--- MNIST/ImageUtils.py
+++ MNIST/ImageUtils.py
@@ -3,4 +3,5 @@ import matplotlib.pyplot as plt
 def show_mnist_image(data):
     if data.shape != (28, 28):
         raise Exception ("'data' shape is {} when expected to be (28, 28)".format(data.shape))
-    plt.imshow(data, cmap='gray', interpolation='nearest') 
+    _, axes = plt.subplots()
+    axes.imshow(data, cmap='gray', interpolation='nearest')
